4cefbd62a80843192f3e51818a09eac6998201a4
pawel0705 <32631610+pawel0705@users.noreply.github.com>
2020-03-22 12:41:53 +0100

    Połączenie z bazą danych - code first

---
+++ InformacjeTurystyczne/InformacjeTurystyczne/Models/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore; // DbContext
+
+namespace InformacjeTurystyczne.Models
+{
+    /*
+     * Obsługa EF (Entity Framework) Core
+     * Klasa stanowi Context Bazy danych
+     * Pośrednik między kodem a rzeczywistą bazą danych (agent ruchu)
+     * Musi dziedziczyć po DbContext
+     * 
+     * Misi być instancja DbContextOption, bo nie zadziała (przesłaniamy metodę configuration, lub przekazujemy przez argument konstruktora)
+     */
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            // ustalamy które typy będą określone w rzeczywistej bazie danych
+            // każdy typ jaki będzie określony w bazie danych jako tabela określamy jako DbSet
+        }
+
+        // DbSet<Model np Schronisko> Schroniska {get; set;}
+        // EF Core jest świadomy istnienia modelu Schronisko i utworzy odpowiednią tabelę
+        // pozniej oczywiście tworzymy model Schronisko (przechowuje stuff typu int id, string cośtam itp)
+        // Następnie tworzymy Repozytorium SchroniskoRepository, które dziedziczy po interfejsie ISchroniskoRepository
+        // Interfejs ISchroniskoRepository np może mieć metody public Schrnisko PobierzSchroniskoId(int schroniskoId) lub public IEnumerable<Schronisko> PobierzWszystkieSchroniska() itp (takie zapytania gotowce)
+        // w SchroniskoRepository musimy mieć dostęp do AppDbContext
+        /*
+         * private readonly AppDbContext _appDbContext;
+         * 
+         * ten appDbContext uzyskujemy poprzez "wstrzykiwanie zależności" konstruktora
+         * 
+         * public SchroniskoRepository(AppDbContext appDbContext)
+         * {
+         *  _appContext = appContext;
+         * }
+         * 
+         * później implementujemy interfejs
+         * np dla wszystkich schronisk // Schroniska to kolekcja utworzona wcześniej
+         * return _appDbContext.Schroniska; // za kulisamu sprawdza różne rzeczy automatycznie
+         * 
+         * np dla danego id
+         * return _appDbContext.Schrniska.FirstOrDefault(s=> s.Id = schroniskId);
+         */
+
+    }
+}
--- InformacjeTurystyczne/InformacjeTurystyczne/Startup.cs
+++ InformacjeTurystyczne/InformacjeTurystyczne/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InformacjeTurystyczne.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,16 +15,24 @@ namespace InformacjeTurystyczne
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; } 
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; // odczytujemy konfiguracje przez klasê Program.cs
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // musimy zarejestrowaæ w kolekcji us³ug, ¿e bêdziemy korzystaæ z EF Core
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); // rejestrowanie naszego contextu i deklaracja, ¿e chcemy u¿ywaæ SQL Server
+            // jak nie dzia³a to w konsoli menad¿era pakietów wklepujemy PM > Install-Package Microsoft.EntityFrameworkCore.SqlServer
+            // potrzebujemy ci¹gu po³¹czenia by po³¹czyæ siê z rzeczywist¹ baz¹ danych (connection string), jest on w appsettings.json
+
+            // póŸniej robimy coœ w stulu
+            // services.AddTransient<ISchroniskoRepository, SchroniskoRepository>();
+
             services.AddControllersWithViews();
         }
 
