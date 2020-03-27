d89a16b90d95652646a4119472617138dc0f9aa7
pawel0705 <32631610+pawel0705@users.noreply.github.com>
2020-03-27 22:46:38 +0100

    Logowanie kontem google + próba custom redirections
    Custom redirections typu customowe przejście do strony z logowaniem (jeżeli chce sie przejść do czegoś co wymaga zalogowania) lub do własnej strony błędu 404 nie chce działać i nie wiem dladczego ;-;

--- InformacjeTurystyczne/InformacjeTurystyczne/Startup.cs
+++ InformacjeTurystyczne/InformacjeTurystyczne/Startup.cs
@@ -20,7 +20,7 @@ namespace InformacjeTurystyczne
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; } 
+        public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
         {
@@ -30,21 +30,30 @@ namespace InformacjeTurystyczne
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.Configure<CookieAuthenticationOptions>(options =>
             {
                 options.LoginPath = new PathString("/Account/Login");
                 options.AccessDeniedPath = new PathString("/Account/AccessDenied");
-        }); 
+
+            });
 
 
 
             // musimy zarejestrowaæ w kolekcji us³ug, ¿e bêdziemy korzystaæ z EF Core
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); // rejestrowanie naszego contextu i deklaracja, ¿e chcemy u¿ywaæ SQL Server
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // rejestrowanie naszego contextu i deklaracja, ¿e chcemy u¿ywaæ SQL Server
             // jak nie dzia³a to w konsoli menad¿era pakietów wklepujemy PM > Install-Package Microsoft.EntityFrameworkCore.SqlServer
             // potrzebujemy ci¹gu po³¹czenia by po³¹czyæ siê z rzeczywist¹ baz¹ danych (connection string), jest on w appsettings.json
-
             // póŸniej robimy coœ w stulu
             // services.AddTransient<ISchroniskoRepository, SchroniskoRepository>();
+
+
+            services.AddAuthentication().AddGoogle(options =>
+            {
+                options.ClientId = "278465396718-e4gkbb2to6lmjn5lq85j2m3d2oukr48k.apps.googleusercontent.com";
+                options.ClientSecret = "kZwtCzF48bQ0ytZPckBk3yCJ";
+            });
 
             services.AddTransient<ICategoryRepository, CategoryRepository>();
 
@@ -52,14 +61,15 @@ namespace InformacjeTurystyczne
 
             services.AddIdentity<AppUser, IdentityRole>(config =>
             {
-                config.Password.RequiredLength = 8;
+                config.Password.RequiredLength = 6;
                 config.Password.RequireDigit = true;
                 config.Password.RequireNonAlphanumeric = false;
                 config.Password.RequireUppercase = false;
                 config.SignIn.RequireConfirmedEmail = false;
-            }).AddEntityFrameworkStores<AppDbContext>();
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,9 +81,7 @@ namespace InformacjeTurystyczne
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseStatusCodePagesWithRedirects("/error/{0}");
             }
 
             app.UseStatusCodePages();
@@ -90,15 +98,8 @@ namespace InformacjeTurystyczne
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Admin}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
-            
         }
     }
 }
