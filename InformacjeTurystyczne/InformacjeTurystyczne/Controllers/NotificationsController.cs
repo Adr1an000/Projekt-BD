42336b418c8f7a973fcbabcad59708e8e992ba39
Adr1an000 <32631648+Adr1an000@users.noreply.github.com>
2020-05-14 09:20:01 +0200

    Login+register+notifications

--- InformacjeTurystyczne/InformacjeTurystyczne/Controllers/NotificationsController.cs
+++ InformacjeTurystyczne/InformacjeTurystyczne/Controllers/NotificationsController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using InformacjeTurystyczne.Models.InterfaceRepository;
+using InformacjeTurystyczne.Models.ViewModels;
 
 namespace InformacjeTurystyczne.Controllers
 {
     public class NotificationsController : Controller
     {
+        private readonly IMessageRepository _messageRepository;
+
+        public NotificationsController(IMessageRepository messageRepository)
+        {
+            _messageRepository = messageRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            var viewModel = new NotificationsVM();
+            viewModel.messages = _messageRepository;
+
+            return View(viewModel);
         }
     }
 }
