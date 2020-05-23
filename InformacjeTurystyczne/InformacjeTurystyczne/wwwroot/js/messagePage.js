9b35fc2e6112f8be6aa6b7a58b208bb0ad0c3ac8
Adr1an000 <32631648+Adr1an000@users.noreply.github.com>
2020-05-23 13:17:31 +0200

    message filtering

---
+++ InformacjeTurystyczne/InformacjeTurystyczne/wwwroot/js/messagePage.js
@@ -0,0 +1,33 @@
+﻿import Filter from "./filter.js";
+
+const InfoPage = function (items, regions) {
+    this.regions = regions;
+    this.items = items;
+    this.regionFilter = new Filter("Region", "region", ...regions);
+
+    this.regionFilter.onDirty = () => {
+        this.renderFilters();
+        this.renderItems();
+    };
+};
+
+InfoPage.prototype.renderItems = function () {
+    let fragment = document.createDocumentFragment();
+    let list = document.getElementById("info__list");
+    list.textContent = '';
+    for (let item of this.items.filter(item => this.regionFilter.check(item.region))) {
+        fragment.appendChild(item.render());
+    }
+    list.appendChild(fragment);
+};
+
+InfoPage.prototype.renderFilters = function () {
+    let sidebar = document.getElementsByClassName("sidebar")[0];
+    for (let child of sidebar.querySelectorAll(".expand")) {
+        sidebar.removeChild(child);
+    }
+
+    sidebar.prepend(this.regionFilter.render());
+}
+
+export default InfoPage;
